7555788cc264cb4a731430d2ba0f388ad92aa6f8
PeY Greatz <peygreatz@gmail.com>
2018-02-16 16:58:50 +0700

    Catching exceptions when removing related resources

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -82,6 +82,14 @@ class Handler extends ExceptionHandler
             return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
         }
 
+        if ($exception instanceof QueryException) {
+            $errorCode = $exception->errorInfo[1];
+
+            if ($errorCode == 1451) {
+                return $this->errorResponse('Cannot remove this resource permanently. It is related with any other resource', 409)
+            }
+        }
+
         return parent::render($request, $exception);
     }
 
