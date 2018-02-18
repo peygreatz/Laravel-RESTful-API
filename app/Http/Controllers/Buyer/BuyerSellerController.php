3d6ff80a329c3b10eb41f5064852293d33a631ea
PeY Greatz <peygreatz@gmail.com>
2018-02-18 09:25:03 +0700

    Implementing the index method for BuyerSellerController

---
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerSellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        // relasi yang tidak terkoneksi atau melewati tabel relasi
+        $sellers = $buyer->transactions()->with('product.sellers')
+        ->get()
+        // hanya mengambil tabel seller melewati beda relasi tabel
+        ->pluck('product.seller')
+        ->unique('id')
+        ->values();
+
+        return $this->showAll($sellers);
+    }
+}
