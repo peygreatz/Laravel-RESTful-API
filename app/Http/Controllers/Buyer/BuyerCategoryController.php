38917cd58a4d25de95f9e3e3014625f63b991f8b
PeY Greatz <peygreatz@gmail.com>
2018-02-18 09:30:02 +0700

    Implementing the index method for BuyerCategoryController

---
+++ app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        // relasi yang tidak terkoneksi atau melewati tabel relasi
+        $sellers = $buyer->transactions()->with('product.categories')
+        ->get()
+        // hanya mengambil tabel categories melewati beda relasi tabel
+        ->pluck('product.categories')
+        ->collapse()
+        ->unique('id')
+        ->values();
+
+        return $this->showAll($sellers);
+    }
+}
