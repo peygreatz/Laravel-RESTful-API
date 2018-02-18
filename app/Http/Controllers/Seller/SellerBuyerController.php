c112f9d50ef8aeb378df61a2b311767f86c9f8f7
PeY Greatz <peygreatz@gmail.com>
2018-02-18 10:02:32 +0700

    Implementing the index method for SellerBuyerController

---
+++ app/Http/Controllers/Seller/SellerBuyerController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Seller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class SellerBuyerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Seller $seller)
+    {
+        $buyers = $seller->products()
+            // tampilkan produk yang sudah ada transaksi
+            ->whereHas('transactions')
+            // tampilkan juga transaksi dan buyer
+            ->with('transactions.buyer')
+            ->get()
+            ->pluck('transactions')
+            ->collapse()
+            ->pluck('buyer')
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($buyers);
+    }
+}
